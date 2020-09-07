61add26d3717e6634f08836d44660d1848d0ee8e
zaferdurkut_ff <zafer@fireflyon.com>
2020-09-07 08:51:42 +0300

    tests were updated

--- tests/integration/broker/test_broker_controller.py
+++ tests/integration/broker/test_broker_controller.py
@@ -8,6 +8,7 @@ from run import app
 client = app.test_client()
 
 POST_BOKER_PATH = "/api/brokers/"
+POST_AGECNY_PATH = "/api/agencies/"
 
 
 class CommonController(TestCase):
@@ -17,12 +18,41 @@ class CommonController(TestCase):
         self.client = FlaskClient(self.app)
 
     def test_set_broker(self):
-        # response = client.post(POST_BOKER_PATH)
+        # agency_post_response = client.post(POST_AGECNY_PATH,
+        #                                    json={"address": "Istanbul",
+        #                                                     "domain": "testdomain.com",
+        #                                                     "title": "Test Title"
+        #                                                     },
+        #                                    content_type='application/json')
         #
-        # assert response.status_code == 200
+        # assert agency_post_response.status_code == 200
         #
-        # response_data = json.loads(response.data)
-        # assert response_data == {"status": True}
+        # agency_post_response_data = json.loads(agency_post_response.data)
+        #
+        # test_agency_id = agency_post_response_data["id"]
+        #
+        # broker_post_response = client.post(POST_BOKER_PATH,
+        #                                    json={"first_name": "Zafer",
+        #                                                      "last_name": "Durkut",
+        #                                                      "email": "zafer11@testdomain.com",
+        #                                                      "address": "Ankara"
+        #                                                      },
+        #                                    content_type='application/json')
+        #
+        # assert broker_post_response.status_code == 200
+        #
+        # broker_post_response_data = json.loads(broker_post_response.data)
+        #
+        # test_broker_agency_id = broker_post_response_data["agency_id"]
+        #
+        # assert test_agency_id == test_broker_agency_id
+        #
+        # delete_response = client.delete(POST_BOKER_PATH + str(broker_post_response_data["id"]))
+        #
+        # assert delete_response.status_code == 200
+        #
+        # delete_response = client.delete(POST_AGECNY_PATH + str(test_agency_id))
+        #
+        # assert delete_response.status_code == 200
+
         assert 1 == 1
-
-
